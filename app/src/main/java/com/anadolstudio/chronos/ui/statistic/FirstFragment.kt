f5d201e7bc0e45e9bf10d5c79b3f928032deb994
Anadol <74777850+AnadolStudio@users.noreply.github.com>
2022-09-06 02:08:04 +0300

    delete unused code and use base classes

--- app/src/main/java/com/anadolstudio/chronos/ui/statistic/FirstFragment.kt
+++ app/src/main/java/com/anadolstudio/chronos/ui/statistic/FirstFragment.kt
@@ -1,27 +1,28 @@
-package com.anadolstudio.chronos.ui
+package com.anadolstudio.chronos.ui.statistic
 
 import android.os.Bundle
 import android.view.View
 import com.anadolstudio.chronos.R
-import com.anadolstudio.chronos.databinding.FragmentSecondBinding
+import com.anadolstudio.chronos.databinding.FragmentFirstBinding
 import com.anadolstudio.chronos.navigation.Screens
 import com.anadolstudio.chronos.extensions.navigate
+import com.anadolstudio.chronos.ui.SimpleViewState
 import com.anadolstudio.core.fragment.CoreBaseFragment
 import com.anadolstudio.core.viewbinding.viewBinding
 
-class SecondFragment: CoreBaseFragment<SimpleViewState>(R.layout.fragment_second){
+class FirstFragment : CoreBaseFragment<SimpleViewState>(R.layout.fragment_first) {
 
     override fun render(state: SimpleViewState) {
         TODO("Not yet implemented")
     }
 
-    private val binding by viewBinding(FragmentSecondBinding::bind)
+    private val binding by viewBinding(FragmentFirstBinding::bind)
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        binding.buttonSecond.setOnClickListener {
-            navigate(Screens.FIRST_SCREEN)
+        binding.buttonFirst.setOnClickListener {
+            navigate(Screens.SECOND_SCREEN)
         }
     }
 
--- app/src/main/java/com/anadolstudio/chronos/ui/today/SecondFragment.kt
+++ app/src/main/java/com/anadolstudio/chronos/ui/today/SecondFragment.kt
@@ -1,27 +1,28 @@
-package com.anadolstudio.chronos.ui
+package com.anadolstudio.chronos.ui.today
 
 import android.os.Bundle
 import android.view.View
 import com.anadolstudio.chronos.R
-import com.anadolstudio.chronos.databinding.FragmentFirstBinding
+import com.anadolstudio.chronos.databinding.FragmentSecondBinding
 import com.anadolstudio.chronos.navigation.Screens
 import com.anadolstudio.chronos.extensions.navigate
+import com.anadolstudio.chronos.ui.SimpleViewState
 import com.anadolstudio.core.fragment.CoreBaseFragment
 import com.anadolstudio.core.viewbinding.viewBinding
 
-class FirstFragment : CoreBaseFragment<SimpleViewState>(R.layout.fragment_first) {
+class SecondFragment: CoreBaseFragment<SimpleViewState>(R.layout.fragment_second){
 
     override fun render(state: SimpleViewState) {
         TODO("Not yet implemented")
     }
 
-    private val binding by viewBinding(FragmentFirstBinding::bind)
+    private val binding by viewBinding(FragmentSecondBinding::bind)
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        binding.buttonFirst.setOnClickListener {
-            navigate(Screens.SECOND_SCREEN)
+        binding.buttonSecond.setOnClickListener {
+            navigate(Screens.FIRST_SCREEN)
         }
     }
 
