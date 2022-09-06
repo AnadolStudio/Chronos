da7fc02f30861ebf2df26b5fa66251f2ec4183de
Anadol <74777850+AnadolStudio@users.noreply.github.com>
2022-09-07 00:19:06 +0300

    create CategoryDatabase

--- data/src/main/java/com/anadolstudio/data/data/Category.kt
+++ data/src/main/java/com/anadolstudio/data/data/Category.kt
@@ -1,8 +1,18 @@
 package com.anadolstudio.data.data
 
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = Category.CATEGORY_TABLE)
 data class Category(
-    val name: String,
-    val categoryId: Int,
-    val parentId: String,
-    val totalMinutes: String
-)
+    @PrimaryKey(autoGenerate = true) val id: Int,
+    @ColumnInfo(name = "category_id") val categoryId: String,
+    @ColumnInfo(name = "name") val name: String,
+    @ColumnInfo(name = "total_minutes") val totalMinutes: String,
+    @ColumnInfo(name = "child_id") val childId: String?
+) {
+    companion object {
+        const val CATEGORY_TABLE = "category_table"
+    }
+}
--- data/src/main/java/com/anadolstudio/data/repository/CategoryRepository.kt
+++ data/src/main/java/com/anadolstudio/data/repository/CategoryRepository.kt
@@ -4,14 +4,18 @@ import com.anadolstudio.data.data.Category
 
 interface CategoryRepository {
 
-    fun addCategory(categoryId: String, name: String, minuteCount: Int, parent: String? = null): Category
+    // TODO Подумай лучше
 
-    fun setCategory(categoryId: String, name: String, minuteCount: Int, parent: String? = null): Category
+    fun insertCategory(category: Category)
+
+    fun updateCategory(category: Category)
 
     fun renameCategory(categoryId: String, newName: String): Category
 
     fun getAllCategories(): List<Category>
 
     fun getCategory(categoryId: String): Category
+
+    fun deleteCategory(category: Category)
 
 }
