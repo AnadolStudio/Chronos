5d7ed31b14d5f2e65f3c2968cc203be801ec87ba
Anadol <74777850+AnadolStudio@users.noreply.github.com>
2023-01-03 19:49:17 +0300

    done CategoryDao

---
+++ domain/src/main/java/com/anadolstudio/domain/database/category/CategoryDao.kt
@@ -0,0 +1,29 @@
+package com.anadolstudio.domain.database.category
+
+import androidx.room.*
+import com.anadolstudio.domain.model.category.CategoryEntity
+import com.anadolstudio.domain.model.category.CategoryEntity.Companion.CATEGORY_TABLE
+import java.util.UUID
+
+@Dao
+interface CategoryDao {
+
+    @Query("SELECT * FROM $CATEGORY_TABLE")
+    fun getAllCategories(): List<CategoryEntity>
+
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    fun insertCategory(category: CategoryEntity)
+
+    @Query("UPDATE $CATEGORY_TABLE SET name = :name, color = :color  WHERE category_id = :categoryId")
+    fun updateCategory(categoryId: UUID, name: String, color: Int): Int
+
+    @Query("SELECT * FROM $CATEGORY_TABLE WHERE category_id = :categoryId")
+    fun getCategoryById(categoryId: String): CategoryEntity
+
+    @Query("SELECT * FROM $CATEGORY_TABLE WHERE name = :categoryName")
+    fun getCategoryByName(categoryName: String): List<CategoryEntity>
+
+    @Delete
+    fun deleteCategory(category: CategoryEntity)
+
+}
